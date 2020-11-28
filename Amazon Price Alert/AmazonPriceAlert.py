8b9b395000bea68b74fe6c9de17c5ff752d76be4
vishal775 <56577316+vishal775@users.noreply.github.com>
2020-11-28 13:46:26 +0530

    Update AmazonPriceAlert.py

--- Amazon Price Alert/AmazonPriceAlert.py
+++ Amazon Price Alert/AmazonPriceAlert.py
@@ -1,3 +1,4 @@
+# Before running this code go to www.twilio.com and create a account with your PHONE NUMBER and get a TRIAL NUMBER for your account by your choice.
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -5,7 +6,12 @@ import time
 from twilio.rest import Client
 import pickle
 
+# Choose the Product and copy the Product URL and paste it in site = "Product URL" given below
 site = "https://www.amazon.in/OnePlus-Mirror-Black-128GB-Storage/dp/B085J1CPD1/ref=lp_22429860031_1_2?s=electronics&ie=UTF8&qid=1606104936&sr=1-2"
+
+# Download Chrome Driver by checking the version of the chrome you are using 
+# After Download extract in Local Disc C if possible or any other Local Disc.
+# Now get the whole PATH of chromediver.exe in extracted directory and paste it in "User-Agent": "YOUR CHROMEDRIVER.EXE PATH"
 headers = {
      "User-Agent": "C:/chromedriver.exe"
 }
@@ -24,10 +30,13 @@ def check_price():
      converted_price = int(to_convert_price)
      return converted_price
 
+# Enter your Phone Number.
+# Get the YOUR_ACCOUNT_SID and YOUR_AUTH_TOKEN by logging in your Twilio Account and paste in Client("YOUR_ACCOUNT_SID", "YOUR_AUTH_TOKEN") mentioned below.
+# Get your Trail Number which you can see in the Dashboard and enter it in from_='YOUR_TRIAL_NUMBER' given below.
 def msg(password):
-     num='+919944446313'
-     client = Client("AC47902c9d8bcc3e1388601d2595023044", "bcc68cd2d28f9ef27e9fc0e2d3be6da5")
-     client.messages.create(from_='+17706912470',
+     num='+91'+'YOUR_PHONE_NUMBER'
+     client = Client("YOUR_ACCOUNT_SID", "YOUR_AUTH_TOKEN")
+     client.messages.create(from_='YOUR_TRIAL_NUMBER',
                            to=num,
                            body='The Price is : '+password)
     
@@ -46,11 +55,14 @@ def get_price(price):
      else:
           pr='Same Price '+str(price)
           print(pr)
-          msg(pr)
-          print('Message sent')
+          
+          # If needed you can get the Message for the same price also 
+          #msg(pr)
+          #print('Message sent')
+          
 a=check_price()
 
 while True:
      count=0
      get_price(a)
-     time.sleep(60)
+     time.sleep(60) # You can change the 60 according to your needed refresh time of your Product so that it gives the exact price of it.
